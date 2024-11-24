1663d62d6f63739814b7c5edd480759d4789d6ef
zainuldeen <78583049+Zain-ul-din@users.noreply.github.com>
2024-11-25 04:07:14 +0500

    redirects

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -2,7 +2,7 @@ import { SignIn } from "@clerk/nextjs";
 
 export default function SignInPage() {
   return (
-    <main className="w-full h-[100svh] flex justify-center items-center">
+    <main className="w-full h-[100svh] flex justify-center items-center bg-indigo-300">
       <SignIn />
     </main>
   );

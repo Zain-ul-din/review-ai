67fe822d21d86db3fde91d49a6bac9038785d597
zainuldeen <78583049+Zain-ul-din@users.noreply.github.com>
2024-11-27 00:02:12 +0500

    add confetti

--- src/components/campaign/index.tsx
+++ src/components/campaign/index.tsx
@@ -14,8 +14,58 @@ import {
   DialogTitle,
   DialogTrigger
 } from "../ui/dialog";
+import confetti from "canvas-confetti";
 
 export default function Campaign() {
+  const handleClick = () => {
+    const scalar = 2;
+    const triangle = confetti.shapeFromPath({
+      path: "M0 10 L5 0 L10 10z"
+    });
+    const square = confetti.shapeFromPath({
+      path: "M0 0 L10 0 L10 10 L0 10 Z"
+    });
+    const coin = confetti.shapeFromPath({
+      path: "M5 0 A5 5 0 1 0 5 10 A5 5 0 1 0 5 0 Z"
+    });
+    const tree = confetti.shapeFromPath({
+      path: "M5 0 L10 10 L0 10 Z"
+    });
+
+    const defaults = {
+      spread: 360,
+      ticks: 60,
+      gravity: 0,
+      decay: 0.96,
+      startVelocity: 20,
+      shapes: [triangle, square, coin, tree],
+      scalar
+    };
+
+    const shoot = () => {
+      confetti({
+        ...defaults,
+        particleCount: 30
+      });
+
+      confetti({
+        ...defaults,
+        particleCount: 5
+      });
+
+      confetti({
+        ...defaults,
+        particleCount: 15,
+        scalar: scalar / 2,
+        shapes: ["circle"]
+      });
+    };
+
+    setTimeout(shoot, 0);
+    setTimeout(shoot, 100);
+    setTimeout(shoot, 200);
+  };
+
   return (
     <main
       className={cn(
@@ -38,6 +88,10 @@ export default function Campaign() {
           OrbisDev
         </h1>
 
+        {/* <p className="text-2xl text-center py-8">
+          Thank You for Your Feedback! 🙌
+        </p> */}
+
         <p className="text-lg md:text-xl px-1">
           We’d love to hear your thoughts! Help us improve by sharing your
           experience with OrbisDev.
@@ -45,7 +99,11 @@ export default function Campaign() {
 
         <div className="flex">
           <div className="mx-auto">
-            <Dialog>
+            <Dialog
+              onOpenChange={(open) => {
+                if (!open) handleClick();
+              }}
+            >
               <DialogTrigger>
                 <TextureButton>Submit reviews</TextureButton>
               </DialogTrigger>
@@ -69,6 +127,37 @@ export default function Campaign() {
 export const FeedbackForm = () => {
   const form = useForm();
 
+  const handleClick = () => {
+    const defaults = {
+      spread: 360,
+      ticks: 50,
+      gravity: 0,
+      decay: 0.94,
+      startVelocity: 30,
+      colors: ["#FFE400", "#FFBD00", "#E89400", "#FFCA6C", "#FDFFB8"]
+    };
+
+    const shoot = () => {
+      confetti({
+        ...defaults,
+        particleCount: 40,
+        scalar: 1.2,
+        shapes: ["star"]
+      });
+
+      confetti({
+        ...defaults,
+        particleCount: 10,
+        scalar: 0.75,
+        shapes: ["circle"]
+      });
+    };
+
+    setTimeout(shoot, 0);
+    setTimeout(shoot, 100);
+    setTimeout(shoot, 200);
+  };
+
   return (
     <Form {...form}>
       <form className="space-y-8">
@@ -90,6 +179,9 @@ export const FeedbackForm = () => {
                         className={cn(
                           "bg-muted p-2 border cursor-pointer border-muted-foreground/40 rounded-md"
                         )}
+                        onClick={() => {
+                          if (i == 4) handleClick();
+                        }}
                       >
                         <Star
                           className={cn(

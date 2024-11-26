3099726bfb3d676d4bf84509ccc03be090fba0ba
zainuldeen <78583049+Zain-ul-din@users.noreply.github.com>
2024-11-26 23:32:59 +0500

    move feedback to a dialog

---
+++ src/components/campaign/index.tsx
@@ -0,0 +1,145 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import { Form, FormField, FormItem, FormLabel } from "@/components/ui/form";
+import { useForm } from "react-hook-form";
+import { Star } from "lucide-react";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { TextureButton } from "@/components/ui/texture-button";
+import { Avatar, AvatarImage } from "../ui/avatar";
+import {
+  Dialog,
+  DialogContent,
+  DialogTitle,
+  DialogTrigger
+} from "../ui/dialog";
+
+export default function Campaign() {
+  return (
+    <main
+      className={cn(
+        "w-full h-[100svh] bg-gradient-to-bl from-indigo-100/90  via-pink-200/60 to-indigo-200/80",
+        "flex justify-center items-center p-4"
+      )}
+    >
+      <div
+        className={cn(
+          "max-w-[550px]",
+          "rounded-lg  w-full bg-gradient-to-bl from-background via-background/80 to-background",
+          "border-2 p-6 sm:p-12 relative",
+          "space-y-6"
+        )}
+      >
+        <h1 className="text-2xl flex items-center gap-2">
+          <Avatar>
+            <AvatarImage src="/temp/orbisdev-logo.png" alt="orbisdev logo" />
+          </Avatar>
+          OrbisDev
+        </h1>
+
+        <p className="text-lg md:text-xl px-1">
+          We’d love to hear your thoughts! Help us improve by sharing your
+          experience with OrbisDev.
+        </p>
+
+        <div className="flex">
+          <div className="mx-auto">
+            <Dialog>
+              <DialogTrigger>
+                <TextureButton>Submit reviews</TextureButton>
+              </DialogTrigger>
+              <DialogContent>
+                <DialogTitle className="sr-only">
+                  Submit Review Dialog
+                </DialogTitle>
+                <FeedbackForm />
+              </DialogContent>
+            </Dialog>
+          </div>
+        </div>
+
+        {/* <FeedbackForm /> */}
+        {/* <h1>OrbisDev</h1> */}
+      </div>
+    </main>
+  );
+}
+
+export const FeedbackForm = () => {
+  const form = useForm();
+
+  return (
+    <Form {...form}>
+      <form className="space-y-8">
+        <FormField
+          name="rating"
+          control={form.control}
+          render={() => {
+            return (
+              <FormItem className="space-y-4">
+                <FormLabel className="text-2xl font-medium">
+                  Overall Rating
+                </FormLabel>
+
+                <div className="flex gap-4">
+                  {new Array(5).fill(0).map((v, i) => {
+                    return (
+                      <span
+                        key={i}
+                        className={cn(
+                          "bg-muted p-2 border cursor-pointer border-muted-foreground/40 rounded-md"
+                        )}
+                      >
+                        <Star
+                          className={cn(
+                            i < 4 && "text-yellow-500 fill-yellow-300"
+                          )}
+                        />
+                      </span>
+                    );
+                  })}
+                </div>
+              </FormItem>
+            );
+          }}
+        />
+
+        <FormField
+          name="title"
+          control={form.control}
+          render={() => (
+            <FormItem>
+              <FormLabel className="text-lg">Review title</FormLabel>
+              <Input placeholder="Example: Easy to use" className="py-6" />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          name="review"
+          control={form.control}
+          render={() => (
+            <FormItem>
+              <FormLabel className="text-lg">Review</FormLabel>
+              {/* <div className="flex flex-wrap items-end text-lg ">
+              Since bought this{" "}
+              <input
+                placeholder="product"
+                className="ml-2 outline-0 text-inherit w-[100px] focus:border-b"
+              />{" "}
+              im feeling happy.
+            </div> */}
+              <Textarea
+                placeholder="Example: This product exceeded my expectations! Great quality and fast shipping."
+                className="py-4 max-h-[200px]"
+              />
+            </FormItem>
+          )}
+        />
+
+        <TextureButton size="lg">Submit</TextureButton>
+      </form>
+    </Form>
+  );
+};
